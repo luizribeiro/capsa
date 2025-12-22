0b09b97ca03b0cd5c19e54658e9d397b0ade0b0e
Luiz Ribeiro <luizribeiro@gmail.com>
2025-12-21 23:37:34 -0500

    Make devenv.nix work on Linux by conditionalizing macOS-only packages

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   languages.rust.enable = true;
   languages.rust.channel = "nightly";
 
-  packages = [
+  packages = lib.optionals pkgs.stdenv.isDarwin [
     pkgs.vfkit
   ];
 
@@ -16,7 +16,7 @@
 
   # codesign-run wraps binary execution with ad-hoc codesigning using virtualization
   # entitlements, required for running tests and examples that use Virtualization.framework
-  enterShell = ''
+  enterShell = lib.optionalString pkgs.stdenv.isDarwin ''
     if ! command -v codesign-run &> /dev/null; then
       echo "Installing codesign-run..."
       cargo install --git https://github.com/luizribeiro/apple-main codesign-run
