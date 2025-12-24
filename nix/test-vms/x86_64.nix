67221815111948e6b14847f72e874ab0be9cd368
Luiz Ribeiro <luizribeiro@gmail.com>
2025-12-24 09:04:46 -0500

    organize x86_64.nix a bit

--- nix/test-vms/x86_64.nix
+++ nix/test-vms/x86_64.nix
@@ -8,42 +8,35 @@ let
   pkgs = import nixpkgs { system = "x86_64-linux"; };
   vmLib = import ./lib.nix { inherit pkgs; };
 
-  linuxArch = "x86";
   kernelImage = "bzImage";
   kernelTarget = "bzImage";
-  console = "ttyS0";
 
-  kernelConfig = {
-    X86_64 = true;
-    SERIAL_8250 = true;
-    SERIAL_8250_CONSOLE = true;
-  };
-
-  universalKernel = vmLib.mkKernel {
+  kernel = vmLib.mkKernel {
     name = "universal";
-    inherit linuxArch kernelImage kernelTarget;
-    config = kernelConfig;
+    linuxArch = "x86";
+    inherit kernelImage kernelTarget;
+    config = {
+      X86_64 = true;
+      SERIAL_8250 = true;
+      SERIAL_8250_CONSOLE = true;
+    };
   };
 
-  universalInitrd = vmLib.mkInitrd { inherit console; };
-
-  vms = {
-    default = vmLib.mkDirectBootVm {
-      name = "default";
-      kernel = universalKernel;
-      inherit kernelImage;
-      initrd = universalInitrd;
-    };
-    with-disk = vmLib.mkDirectBootVm {
-      name = "with-disk";
-      kernel = universalKernel;
-      inherit kernelImage;
-      initrd = universalInitrd;
-      disk = { sizeMB = 32; };
-    };
+  initrd = vmLib.mkInitrd {
+    console = "ttyS0";
   };
 in
 vmLib.mkCombined {
   name = "x86_64";
-  inherit vms;
+  vms = {
+    default = vmLib.mkDirectBootVm {
+      name = "default";
+      inherit kernel kernelImage initrd;
+    };
+    with-disk = vmLib.mkDirectBootVm {
+      name = "with-disk";
+      inherit kernel kernelImage initrd;
+      disk = { sizeMB = 32; };
+    };
+  };
 }
